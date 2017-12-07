878af4c46ecd998b612b82ed5d8c3b63be0a7a04
Alin-Gheorghe Balutoiu <abalutoiu@cloudbasesolutions.com>
2017-12-07 12:43:51 +0100

    Fix pep8 errors for python code

--- python/client.py
+++ python/client.py
@@ -1,30 +1,27 @@
 #!/usr/bin/env python2.7
-import imp
 import os
 import sys
 
-
-import omi
 from omi import *
-        
 
-def main (argv = None):
-    be = BookEnd ('main')
-    for i in range (len (argv)):
-        BookEndPrint ('args[' + str (i) + ']: "' + argv[i] + '"')
-    if len (argv) == 4:
+
+def main(argv=None):
+    be = BookEnd('main')
+    for i in range(len(argv)):
+        BookEndPrint('args[' + str(i) + ']: "' + argv[i] + '"')
+    if len(argv) == 4:
         try:
-            path = os.path.split (os.path.realpath (argv[0]))[0] + '/' + argv[1]
-            port = int (argv[2])
-            client = Client (path, port, argv[3])
-            client.run ()
+            path = os.path.split(os.path.realpath(argv[0]))[0] + '/' + argv[1]
+            port = int(argv[2])
+            client = Client(path, port, argv[3])
+            client.run()
         except:
-            e = sys.exc_info ()[0]
-            sys.stderr.write ('Unhandled exception: ' + str(e) + '\n')
+            e = sys.exc_info()[0]
+            sys.stderr.write('Unhandled exception: ' + str(e) + '\n')
             raise
     else:
-        sys.stderr.write ('Usage: client.py [SOCKET_ID] [PROVIDER]\n')
+        sys.stderr.write('Usage: client.py [SOCKET_ID] [PROVIDER]\n')
 
 
 if __name__ == '__main__':
-    sys.exit (main (sys.argv))
+    sys.exit(main(sys.argv))
